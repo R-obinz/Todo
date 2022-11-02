f21a69d7e98db85d9cb63fe51b13243297d08fe6
R-obinz <robinjustinz011@gmail.com>
2022-11-02 17:49:32 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+  
+    import React, { useEffect, useState } from 'react';
+    import './Todo.css';
+    
+    function Task({ task,index,completeTask,removeTask }) {
+        return (
+            <div
+                className="task"
+                style={{ textDecoration: task.completed ? "line-through" : "" }}
+            >
+                {task.title}
+                <button style={{background:"red"}} onClick={()=>removeTask(index)}>x</button>
+                <button onClick={()=>completeTask(index)}>Complete</button>
+                
+            </div>
+        );
+    }
+    function App() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        const [tasksRemaining,setTasksRemaining] = useState(0);
+        const [tasks, setTasks] = useState([
+         
+        ]);
+        
 
-export default App;
+        useEffect(()=>{
+          setTasksRemaining(tasks.filter(task=>!task.completed).length)
+        });
+
+        const addTask = title =>{
+          const newTasks =[...tasks ,{title,completed:false}];
+          setTasks(newTasks);
+        }
+
+        const completeTask = index=>{
+          const newTasks = [...tasks];
+          newTasks[index].completed =true;
+          setTasks(newTasks);
+        }
+
+        const removeTask = index=>{
+          const newTasks = [...tasks];
+          newTasks.splice(index,1);
+          setTasks(newTasks);
+        }
+
+        return (
+            <div className="todo-container">
+                <div className="header">Pending Tasks({tasksRemaining})</div>
+                <div className="tasks">
+                    {tasks.map((task, index) => (
+                        <Task
+                            task={task}
+                            index={index}
+                            completeTask={completeTask}
+                            removeTask={removeTask}
+                            key={index}
+                        />
+                    ))}
+                </div>
+                <div className='create-task'>
+                  <CreateTask addTask={addTask}/>
+                </div>
+            </div>
+            
+        );
+    }
+
+    function CreateTask({ addTask}){
+      const [value,setValue] =useState("");
+
+      const handleSubmit = e =>{
+        e.preventDefault();
+        if(!value)return;
+
+        addTask(value);
+        setValue("");
+      }
+
+      return(
+        <form onSubmit={handleSubmit}>
+          <input type="text"
+          className="input"
+          value={value}
+          placeholder="Add a new task"
+          onChange={e=>setValue(e.target.value)} />
+        </form>
+      )
+    } 
+    
+   
+    
+    export default App;
